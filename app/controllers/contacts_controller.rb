41f4e175cf853c7350885c3354e84cc9379a6a89
Clayton K. N. Passos <netstart@gmail.com>
2018-03-26 01:17:52 -0300

    apagar um telefone pelo contato

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -46,6 +46,6 @@ class ContactsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def contact_params
-    params.require(:contact).permit(:name, :email, :bithdate, :kind_id, phones_attributes: [:id, :number]) # strong parameters
+    params.require(:contact).permit(:name, :email, :bithdate, :kind_id, phones_attributes: [:id, :number, :_destroy]) # strong parameters
   end
 end
